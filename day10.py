88946baceb1fc56703cf17d98e39103dc8d9691b
Mohamed Akbarally <49976583+MohamedAkbarally@users.noreply.github.com>
2022-12-12 15:23:08 +0530

    day 10 completed

--- day10.py
+++ day10.py
@@ -17,4 +17,24 @@ def p1(f):
     return sum([xs[i-1]*i for i in [20,60,100,140,180,220]])
 
 def p2(f):
-    return
+    ys = f.read().splitlines()
+    xs = [1]
+
+    i = 0
+    while i < len(ys):
+        x = xs[-1]
+        instructions = ys[i].split(" ")
+        if instructions[0] == "addx":
+            xs += [x,x+int(instructions[1])]
+        if instructions[0] == "noop":
+            xs += [x]
+        i+=1
+
+    drawing = list(' '*240)
+    for i in range(240):
+        if xs[i]-1 <= (i%40) <= xs[i]+1:
+            drawing[i] = '#'
+
+    print()
+    [print(''.join(drawing[i:i+40])) for i in range(0, len(drawing), 40)]
+    return 'Done'
